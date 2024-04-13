0cbbca38ea951c2d476d36eb3b9667ffdabb20a9
Amaayu <namdevaniket446@gmail.com>
2024-04-14 01:48:48 +0530

    profile page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
@@ -6,17 +7,29 @@ const Footer = () => {
     <>
       <div className="footer">
         <div class="fot-left">
-          <i class="ri-home-6-line"></i>
-          <i class="ri-search-2-line"></i>
+          <NavLink to="/feed" className="log-3">
+            <i class="ri-home-6-line"></i>
+          </NavLink>
+          <NavLink to="/feed" className="log-3">
+            <i class="ri-search-2-line"></i>
+          </NavLink>
         </div>
-        <button className="btn"></button>
+        <NavLink to="/feed" className="log-3">
+          <button className="btn"></button>
+        </NavLink>
+
         <span>pitch-idea</span>
         <div className="fot-right">
-          <i class="ri-notification-2-line"></i>
-          <img
-            src="https://res.cloudinary.com/dijzsv2tt/image/upload/v1702932131/wnjynhj7jft7vzo4frg3.jpg"
-            alt="icon"
-          />
+          <NavLink to="/feed" className="log-3">
+            <i class="ri-notification-2-line"></i>
+          </NavLink>
+
+          <NavLink to="/profile" className="log-2">
+            <img
+              src="https://res.cloudinary.com/dijzsv2tt/image/upload/v1702932131/wnjynhj7jft7vzo4frg3.jpg"
+              alt="icon"
+            />
+          </NavLink>
         </div>
       </div>
     </>
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,10 +1,36 @@
 import "./Profile.css";
 import React from "react";
+import Footer from "../../components/footer/Footer";
 
 const Profile = () => {
   return (
     <>
-      <div className="profile"></div>
+      <div className="profile">
+        <div className="nav-1">
+          <i class="ri-arrow-left-line"></i>
+          <i class="ri-chat-1-line"></i>
+        </div>
+        <div className="mid">
+          <p>My Profile</p>
+          <div className="mid-row">
+            <img
+              src="https://res.cloudinary.com/dijzsv2tt/image/upload/v1702931273/ilaody3w0s6ywvpqggtl.jpg"
+              alt="profile"
+            />
+            <h4>Aayush Tonk</h4>
+            <h6>aayu_is.here</h6>
+            <div className="btn-2"> Edit profile </div>
+          </div>
+          <div className="mid-row-1">
+            <img
+              src="https://res.cloudinary.com/dijzsv2tt/image/upload/v1713036949/s306zoo4j5r1ek3ylfe8.png"
+              alt=""
+            />
+          </div>
+        </div>
+
+        <Footer />
+      </div>
     </>
   );
 };
