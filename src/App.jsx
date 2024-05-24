c29d5168cc6554aeb055d47300bdb8bbc8212330
Amaayu <namdevaniket446@gmail.com>
2024-05-24 18:31:29 +0530

    almost complet

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SignUp from "./pages/singup/Singup";
 import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
 import Feed from "./pages/feed/Feed";
 import Postpage from "./pages/post/Postpage";
 import { Routes, Route } from "react-router-dom";
+import axios from "axios";
 const App = () => {
+  const [Data, setData] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3003").then((res) => {
+      setData(res.data);
+    });
+  }, []);
+  console.log(Data);
   return (
     <>
       <Routes>
