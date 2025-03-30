4b7349d10e5aa4b36793466d46a2e2e48b8ea53d
Amaayu <namdevaniket446@gmail.com>
2025-03-30 22:17:33 +0530

    add api

--- api/api.js
+++ api/api.js
@@ -1,6 +1,6 @@
 //const api = "https://hook4startup.onrender.com"; // ✅ API URL
 //const api = "http://localhost:8081";
 
-const api = "http://localhost:8081";
+const api = "https://hook4startup.onrender.com";
 
 export default api;
